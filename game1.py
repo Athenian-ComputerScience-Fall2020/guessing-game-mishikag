ec2da227b9200552316b6c1eca43c9249cb913ea
mishikag <23mgupta@athenian.org>
2020-09-17 00:48:13 +0000

    finished guessing game

--- game1.py
+++ game1.py
@@ -8,3 +8,25 @@ Use this file to write an "open" version of the game (no test code or defined fo
 "I'm sorry, you are out of guesses."
 
 '''
+import random
+chances = 5
+n = random.randint(0,10)
+print(n)
+guess = int(input("enter a number to guess. you have 5 chances "))
+chances-=1
+while chances > 0:
+    if n < guess:
+        chances-=1
+        print("your guess",guess,"is too high")
+        guess = int(input("enter a number to guess. "))
+    if n > guess:
+        chances-=1
+        print("your guess",guess,"is too low")
+        guess = int(input("enter a number to guess. "))
+    if guess == n:
+        break
+
+if guess == n:
+    print("yay! you guessed the right number which was" ,guess,)
+if guess != n:
+    print("sorry, you ran out of guesses")
