2e8ce732b136f46ccb16b0ae526fc82ffdc4fa86
mishikag <23mgupta@athenian.org>
2020-09-27 06:26:38 +0000

    guessing game

--- game1.py
+++ game1.py
@@ -8,35 +8,51 @@ collaborators: none
 "I'm sorry, you are out of guesses."
 
 '''
-import random
-chances = 5
-n = random.randint(0,10)
-guess = int(input("enter a number to guess. you have 5 chances "))
-chances-=1
-try: 
-    while chances > 0:
+play = True 
+while play:
+  import random
+  range = input("do you want to control the range of the random number? enter yes or no ")
+
+  if range == "yes":
+    start_range = int(input("enter the start of your range "))
+    end_range = int(input("enter the end of your range "))
+    n = random.randint(start_range,end_range)
+  else:
+    n = random.randint(0,10)
+
+  chances_question = input("do you want to control the amount of chances you have? if yes, enter yes. if no, enter no. ")
+
+  if chances_question == "yes":
+    chances = int(input("how many chances do you want "))
+    guess = int(input("enter a number to guess "))
+  elif user_chances == "no":
+    chanes = 5 
+    guess = int(input("enter a number to guess. you have 5 chances "))
+  print(n) #just to test
+  try: 
+    def guessing():
+      while chances != 0:
         if n < guess:
-            chances-=1
-            print("your guess",guess,"is too high")
-            guess = int(input("enter a number to guess. "))
+          chances-=1
+          print("your guess",guess,"is too high")
+          guess = int(input("enter a number to guess. "))
         if n > guess:
-            chances-=1
-            print("your guess",guess,"is too low")
-            guess = int(input("enter a number to guess. "))
+          chances-=1
+          print("your guess",guess,"is too low")
+          guess = int(input("enter a number to guess. "))
         if guess == n:
-            break
-        if guess == "quit":
-            quit = "q"
-        if str(guess) == "q":
-            break
-except:
+          break
+        if chances == 0:
+          print("sorry you ran out of guesses")
+  except:
     print("please enter a number ")
-    int(input("enter a number to guess. you have 5 chances "))
-if str(guess)== "q":
-    print("okay, you are done playing the game")
-if guess == n:
+    int(input("enter a number to guess "))
+    guessing()
+  if guess == n:
     print("yay! you guessed the right number which was" ,guess,)
-if guess != n:
-    print("sorry, you ran out of guesses")
+  play_again = input("do you want to play again? enter yes or no ")
+  if play_again == "no":
+    play = False
+    print("okay you are done playing the guessing game")
 
     
